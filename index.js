05ec2cdb07c9021589352549d55e02539faf0d29
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-09-26 17:37:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 function holidayCountdown(holiday, days) {
   console.log(`${holiday}, ${days}`)
-  return string(`It's ${days} days until ${holiday}!`)
+  return(`It's ${days} days until ${holiday}!`)
 }
