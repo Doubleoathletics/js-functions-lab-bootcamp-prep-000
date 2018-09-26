123f74ce5c8020c96d5e01d861c12b887d441adb
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-09-26 17:12:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function happyHolidays() {
   return ("Happy holidays!")
 }
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+  return console.log
+}
+function happyHolidayTo(name,)
